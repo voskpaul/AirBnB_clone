f775712e437d4de504baf3f4dd542d68ef16bbdc
Schamback <kingoeschamback@gmail.com>
2023-10-15 19:14:44 +0000

    First user

--- models/user.py
+++ models/user.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""Defines the User."""
+"""This module creates a User class"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
-
-    Attributes:
-        email (str): The user mail.
-        password (str): The user password.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-    """
+    """Class for managing user objects"""
 
     email = ""
     password = ""
